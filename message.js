07ad61b40d2da09ad6d0b3d3dcdb9b462d167ae9
DashCruft <theporyafm12@gmail.com>
2021-07-04 17:31:17 -0700

    update

--- message.js
+++ message.js
@@ -1,4 +1,4 @@
-// Put the code below at the end of your message event
+// Put the code below at the end of your message event (replace "bot" with "client" in this code if you use client instead of bot in your index.js)
 const commandfile = bot.commands.get(cmd.slice(prefix.length).toString().toLowerCase()) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length).toString().toLowerCase()));;
 if (commandfile) {
   commandfile.run(bot, message, args);
